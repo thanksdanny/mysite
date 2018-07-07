6fb548bf8184fde5761564e1f7a90dae82088c2b
thanksdanny <hj-07@163.com>
2018-07-07 13:25:31 +0800

    finish part 3

--- polls/urls.py
+++ polls/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
     # added the word 'specifics'
-    path('specifics/<int:question_id>', views.detail, name='detail'),
+    # path('specifics/<int:question_id>', views.detail, name='detail'),
 ]
 
 
